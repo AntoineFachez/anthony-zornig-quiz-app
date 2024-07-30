2b2509fb61dcf5dcdf8aae384117bbc9eb64916f
AntoineFachez <anthony.zornig@gmx.de>
2024-07-30 13:17:19 +0200

    add data/json url

--- scripts/script.js
+++ scripts/script.js
@@ -3,7 +3,9 @@ const showAnswerButton = document.querySelector('.question-card button');
 let data;
 
 document.addEventListener('DOMContentLoaded', function () {
-  fetch('/data/questions.json')
+  fetch(
+    'https://github.com/AntoineFachez/anthony-zornig-quiz-app/blob/main/data/questions.json'
+  )
     .then((response) => response.json())
     .then((fetchedData) => {
       data = fetchedData;
